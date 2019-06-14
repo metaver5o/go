0b537108986e515d362f9bcf290ff9f6844edde2
Marco Matos <marco_aurelio_matos@epam.com>
2019-06-14 14:09:18 +0200

    aes,cipher,md5,rand,hex

---
+++ crypto-project/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/md5"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"io"
+)
+
+func createHash(key string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(key))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+func encrypt(data []byte, passphrase string) []byte {
+	block, _ := aes.NewCipher([]byte(createHash(passphrase)))
+	gcm, _ := cipher.NewGCM(block)
+	nonce := make([]byte, gcm.NonceSize())
+	io.ReadFull(rand.Reader, nonce)
+	ciphertext := gcm.Seal(nonce, nonce, data, nil)
+	return ciphertext
+}
+
+func main() {
+	ciphertext := encrypt([]byte("Hello World"), "password")
+	fmt.Println(string(ciphertext))
+}
